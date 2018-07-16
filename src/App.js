d3bc5bb4ee9c7d071435236db235ddef6416ec94
Francisco Garcia <francisco.garcia.pilar@gmail.com>
2018-07-16 14:27:50 -0500

    Set main App file

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { DrawerNavigator, TabNavigator } from 'react-navigation';
+import { Platform } from 'react-native';
+
+import { Provider } from 'react-redux';
+import {
+    createStore,
+    combineReducers,
+    applyMiddleware
+} from 'redux';
+import thunk from 'redux-thunk';
+import imagesReducer from './reducers/images';
+
+import ImagesList from './screens/ImagesList';
+import MyImages from './screens/MyImages';
+import Camera from './screens/Camera';
+
+let Navigator;
+if (Platform.OS === 'ios') {
+    Navigator = TabNavigator({
+        ImagesList: { screen: ImagesList },
+        MyImages: { screen: MyImages },
+        Camera: { screen: Camera }
+    }, {
+        tabBarOptions: {
+            inactiveTintColor: '#aaa',
+            activeTintColor: '#000',
+            showLabel: false
+        }
+    });
+} else {
+    Navigator = DrawerNavigator({
+        ImagesList: { screen: ImagesList },
+        MyImages: { screen: MyImages },
+        Camera: { screen: Camera }
+    });
+}
+
+let store = createStore(combineReducers({ imagesReducer }), applyMiddleware(thunk));
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <Navigator/>
+            </Provider>
+        );
+    }
+}
